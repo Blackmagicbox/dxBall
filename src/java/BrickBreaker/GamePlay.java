edca7be1464183af11cc96a278c1285f6522410b
Rafael <rafa@blackmagicbox.com>
2022-06-23 00:39:05 +0200

    Implements Collision and bouncing effect

--- src/java/BrickBreaker/GamePlay.java
+++ src/java/BrickBreaker/GamePlay.java
@@ -22,7 +22,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     private int ballposX = 120;
     private int ballposY = 350;
-    private int ballXdir = -1;
+    private int ballXdir = -2;
     private int ballYdir = -2;
 
     public GamePlay() {
@@ -53,6 +53,30 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         timer.start();
+        if (play) {
+            // If ball collides, reverse the direction of the ball.
+            // This is done by multiplying the direction by -1.
+
+            if(new Rectangle(ballposX, ballposY, 20, 30).intersects(new Rectangle(playerX, 560, 100, 8))) { // if ball collides with the player
+                ballYdir = -ballYdir;
+            }
+
+            // If ball collides with the top wall, reverse the direction of the ball.
+            ballposX += ballXdir;
+            ballposY += ballYdir;
+
+            if (ballposX < 0) {
+                ballXdir = -ballXdir;
+            }
+
+            if (ballposY < 0) {
+                ballYdir = -ballYdir;
+            }
+
+            if (ballposX > 670) { // If the ball collides with the right wall, reverse the direction of the ball.
+                ballXdir = -ballXdir;
+            }
+        }
         repaint();
     }
 
