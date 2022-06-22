ff6a1f3094f896ca36d64dfd7608dec56d111b45
Rafael Santos <rafa@blackmagicbox.com>
2022-06-22 23:46:04 +0200

    Feat GamePlayStage
    Move pedal (player)

--- src/java/BrickBreaker/GamePlay.java
+++ src/java/BrickBreaker/GamePlay.java
@@ -1,6 +1,7 @@
 package BrickBreaker;
 
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,12 +12,16 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     private boolean play = false;
     private int score = 0;
+
     private int totalBricks = 21;
+
     private Timer timer;
-    private int delay = 8;
+    private int delay = 30;
+
     private int playerX = 310;
-    private int ballX = 120;
-    private int ballY = 350;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
     private int ballXdir = -1;
     private int ballYdir = -2;
 
@@ -28,14 +33,27 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         timer.start();
     }
 
-    public void print(Graphics graphics) {
+    public void paint(Graphics graphics) {
         graphics.setColor(Color.white);
         graphics.fillRect(1, 1, 692, 592);
+
         graphics.setColor(Color.yellow);
+        graphics.fillRect(0,0,3, 592);
+        graphics.fillRect(0,0,692, 3);
+        graphics.fillRect(691,1,3, 592);
+
+        graphics.setColor(Color.blue);
+        graphics.fillRect(playerX,550,100,8);
+
+        graphics.setColor(Color.green);
+        graphics.fillOval(ballposX, ballposY,20, 20);
+        graphics.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.start();
+        repaint();
     }
 
     @Override
@@ -44,6 +62,30 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX <= 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
+
+    public void moveRight() {
+        play = true;
+        playerX += 20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX -= 20;
     }
 
     @Override
--- src/java/BrickBreaker/Main.java
+++ src/java/BrickBreaker/Main.java
@@ -1,16 +1,18 @@
 package BrickBreaker;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         GamePlay gamePlay = new GamePlay();
-
         frame.setBounds(10, 10, 700, 600);
         frame.setTitle("Brick Breaker");
+        frame.add(gamePlay);
         frame.setResizable(false);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Never add elements to the frame after setting it to visible.
+        frame.setVisible(true);
     }
 }
